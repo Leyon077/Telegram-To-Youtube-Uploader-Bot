9e33e11dd110521fbfc826c2240eff37013023a0
Leyon077 <mathuiyyer@gmail.com>
2024-10-23 20:55:31 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,27 +2,27 @@ import os
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = "7561031757:AAHY9o1PeA0-DDz07gRbegYRol8_VX8ob3s"
 
-    SESSION_NAME = ""
+    SESSION_NAME = "YTLeyonBot"
 
-    API_ID = ""
+    API_ID = "26257599"
 
-    API_HASH = ""
+    API_HASH = "cbdf90a6844992b76e143656f6e07562"
 
-    CLIENT_ID = ""
+    CLIENT_ID = "1073371782269-63puoh8vfl77n33db46ic7r9oqjua2mr.apps.googleusercontent.com"
 
-    CLIENT_SECRET = ""
+    CLIENT_SECRET = "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"
 
-    AUTH_USERS = [942731625]
+    AUTH_USERS = [5387108114]
 
-    VIDEO_DESCRIPTION = ""
+    VIDEO_DESCRIPTION = "LeyonRipz"
 
     VIDEO_CATEGORY = ""
 
     VIDEO_TITLE_PREFIX = ""
 
-    VIDEO_TITLE_SUFFIX = ""
+    VIDEO_TITLE_SUFFIX = "| LeyonRipz"
     
     DEBUG = bool()
 
